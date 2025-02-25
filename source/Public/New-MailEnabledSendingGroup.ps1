7226b69f39471dc55316d12dc5ff8d51800c906b
DrIOS <58635327+DrIOSX@users.noreply.github.com>
2025-02-25 10:32:20 -0600

    Fix: Mail group creation function

--- source/Public/New-MailEnabledSendingGroup.ps1
+++ source/Public/New-MailEnabledSendingGroup.ps1
@@ -3,17 +3,13 @@ function New-MailEnabledSendingGroup {
     param (
         [Parameter(Mandatory = $true)]
         [string]$Name,
-
-        [Parameter(Mandatory = $true)]
+        [Parameter(Mandatory = $false)]
         [string]$Alias,
-
         [Parameter(Mandatory = $true, ParameterSetName = 'CustomDomain')]
         [string]$PrimarySmtpAddress,
-
         [Parameter(Mandatory = $true, ParameterSetName = 'DefaultDomain')]
         [string]$DefaultDomain
     )
-
     # Begin Logging
     if (!($script:LogString)) {
         Write-AuditLog -Start
@@ -21,12 +17,19 @@ function New-MailEnabledSendingGroup {
     else {
         Write-AuditLog -BeginFunction
     }
-
     try {
+        if (!($Alias)) {
+            $Alias = $Name
+        }
         if ($PSCmdlet.ParameterSetName -eq 'DefaultDomain') {
             $PrimarySmtpAddress = "$Alias@$DefaultDomain"
         }
-
+        # Check if the distribution group already exists
+        $existingGroup = Get-DistributionGroup -Identity $Name -ErrorAction SilentlyContinue
+        if ($existingGroup) {
+            Write-AuditLog -Message "Distribution group '$Name' already exists. Returning existing group."
+            return $existingGroup
+        }
         # Create the distribution group
         $groupParams = @{
             Name               = $Name
@@ -34,10 +37,8 @@ function New-MailEnabledSendingGroup {
             PrimarySmtpAddress = $PrimarySmtpAddress
             Type               = "security"
         }
-        # Using EXO.
         Write-AuditLog -Message "Creating distribution group with parameters: $($groupParams | Out-String)"
         $group = New-DistributionGroup @groupParams
-
         Write-AuditLog -Message "Distribution group created: $($group | Out-String)"
         return $group
     }
