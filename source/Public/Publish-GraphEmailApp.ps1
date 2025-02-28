fee048dea7360a84e5a736c74835e930af947cca
DrIOS <58635327+DrIOSX@users.noreply.github.com>
2025-02-28 16:26:35 -0600

    fix: formatting

--- source/Public/Publish-GraphEmailApp.ps1
+++ source/Public/Publish-GraphEmailApp.ps1
@@ -26,32 +26,32 @@
 function Publish-GraphEmailApp {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory = $true, HelpMessage = "The prefix used to initialize the Graph Email App. 2-4 characters letters and numbers only.")]
-        [ValidatePattern("^[A-Z0-9]{2,4}$")]
+        [Parameter(Mandatory = $true, HelpMessage = 'The prefix used to initialize the Graph Email App. 2-4 characters letters and numbers only.')]
+        [ValidatePattern('^[A-Z0-9]{2,4}$')]
         [string]$AppPrefix,
-        [Parameter(Mandatory = $false, HelpMessage = "The thumbprint of the certificate to be retrieved.")]
+        [Parameter(Mandatory = $false, HelpMessage = 'The thumbprint of the certificate to be retrieved.')]
         [string]$CertThumbprint,
-        [Parameter(Mandatory = $true, HelpMessage = "The username of the authorized sender.")]
+        [Parameter(Mandatory = $true, HelpMessage = 'The username of the authorized sender.')]
         [string]$AuthorizedSenderUserName,
-        [Parameter(Mandatory = $true, HelpMessage = "The Mail Enabled Sending Group.")]
+        [Parameter(Mandatory = $true, HelpMessage = 'The Mail Enabled Sending Group.')]
         [string]$MailEnabledSendingGroup
     )
     $PublicMods = `
-        "Microsoft.Graph", "ExchangeOnlineManagement", `
-        "Microsoft.PowerShell.SecretManagement", "SecretManagement.JustinGrote.CredMan"
+        'Microsoft.Graph', 'ExchangeOnlineManagement', `
+        'Microsoft.PowerShell.SecretManagement', 'SecretManagement.JustinGrote.CredMan'
     $PublicVers = `
-        "1.22.0", "3.1.0", `
-        "1.1.2", "1.0.0"
+        '1.22.0', '3.1.0', `
+        '1.1.2', '1.0.0'
     $ImportMods = `
-        "Microsoft.Graph.Authentication", `
-        "Microsoft.Graph.Applications", `
-        "Microsoft.Graph.Identity.SignIns", `
-        "Microsoft.Graph.Users"
+        'Microsoft.Graph.Authentication', `
+        'Microsoft.Graph.Applications', `
+        'Microsoft.Graph.Identity.SignIns', `
+        'Microsoft.Graph.Users'
     $params1 = @{
         PublicModuleNames      = $PublicMods
         PublicRequiredVersions = $PublicVers
         ImportModuleNames      = $ImportMods
-        Scope                  = "CurrentUser"
+        Scope                  = 'CurrentUser'
     }
     if (!($script:LogString)) {
         Write-AuditLog -Start
@@ -59,17 +59,16 @@ function Publish-GraphEmailApp {
     else {
         Write-AuditLog -BeginFunction
     }
-    Write-AuditLog "###############################################"
+    Write-AuditLog '###############################################'
     Initialize-ModuleEnv @params1
     Connect-ToMGGraph
     $AppSettings = Initialize-GraphEmailApp -Prefix "$AppPrefix" -UserId "$AuthorizedSenderUserName"
     $CertDetails = Get-GraphEmailAppCert -AppName $AppSettings.AppName -CertThumbprint $CertThumbprint
     $appRegistration = Register-GraphApp -AppName $AppSettings.AppName -GraphResourceId $AppSettings.graphResourceId -ResID $AppSettings.ResId -CertThumbprint $CertDetails.CertThumbprint
     Get-GraphEmailAppConfig -AppRegistration $appRegistration -GraphServicePrincipalId $AppSettings.GraphServicePrincipal.Id -Context $AppSettings.Context -CertThumbprint $CertDetails.CertThumbprint
-    Read-Host "Provide admin consent now, or copy the url and provide admin consent later. Press Enter to continue."
+    Read-Host 'Provide admin consent now, or copy the url and provide admin consent later. Press Enter to continue.'
     # Call to New-ExchangeEmailAppPolicy
     [void](New-ExchangeEmailAppPolicy -AppRegistration $appRegistration -MailEnabledSendingGroup $MailEnabledSendingGroup)
-    $output = Get-AppSecret -AppName $AppSettings.AppName  -AppRegistration $appRegistration -CertThumbprint $CertDetails.CertThumbprint -Context $AppSettings.Context -User $AppSettings.User -MailEnabledSendingGroup $MailEnabledSendingGroup
+    $output = Get-AppSecret -AppName $AppSettings.AppName -AppRegistration $appRegistration -CertThumbprint $CertDetails.CertThumbprint -Context $AppSettings.Context -User $AppSettings.User -MailEnabledSendingGroup $MailEnabledSendingGroup
     return $output
-    #>
 }
