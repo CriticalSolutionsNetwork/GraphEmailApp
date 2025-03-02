318490ee757ca3011793e6e1eca7ae0c72bd7d7a
DrIOS <58635327+DrIOSX@users.noreply.github.com>
2025-03-02 12:50:37 -0600

    docs: add comment block

--- source/Private/New-EnterpriseAppRegistration.ps1
+++ source/Private/New-EnterpriseAppRegistration.ps1
@@ -1,3 +1,36 @@
+<#
+    .SYNOPSIS
+        Creates a new enterprise application registration in Azure AD with a specified certificate.
+    .DESCRIPTION
+        The New-EnterpriseAppRegistration function creates a new Azure AD application registration (sometimes called
+        an enterprise app) using Microsoft Graph. It sets the sign-in audience, attaches a certificate for authentication,
+        and configures one or more application permission IDs for the specified resource (e.g., Microsoft Graph).
+        Logging is handled by the Write-AuditLog function, and the newly created application object is returned.
+    .PARAMETER DisplayName
+        The display name for the new app registration.
+    .PARAMETER CertThumbprint
+        The thumbprint of the certificate used to secure this app, located in the CurrentUser certificate store.
+    .PARAMETER ResourceAppId
+        The Azure AD resource (for example, the Microsoft Graph app ID: 00000003-0000-0000-c000-000000000000).
+    .PARAMETER PermissionIds
+        One or more permission IDs (application permissions) to grant for the resource. For example, "Mail.Send".
+    .PARAMETER SignInAudience
+        The sign-in audience for the app registration. Valid values are "AzureADMyOrg", "AzureADMultipleOrgs",
+        and "AzureADandPersonalMicrosoftAccount". Defaults to "AzureADMyOrg".
+    .EXAMPLE
+        PS C:\> New-EnterpriseAppRegistration -DisplayName "MyEnterpriseApp" -CertThumbprint "AABBCCDDEEFF1122" -ResourceAppId "00000003-0000-0000-c000-000000000000" -PermissionIds "Mail.Send"
+        Creates a new Azure AD application named "MyEnterpriseApp", attaches the specified certificate, targets the Microsoft Graph
+        resource (AppId 00000003-0000-0000-c000-000000000000), and grants the "Mail.Send" permission.
+    .INPUTS
+        None. You cannot pipe input to this function.
+    .OUTPUTS
+        Microsoft.Graph.PowerShell.Models.MicrosoftGraphApplication
+        Returns the newly created Azure AD application registration object.
+    .NOTES
+        Author: DrIOSx
+        Requires: Microsoft.Graph PowerShell module, Write-AuditLog function
+        The user must have permissions in Azure AD to create and manage applications.
+#>
 function New-EnterpriseAppRegistration {
     [CmdletBinding()]
     param (
